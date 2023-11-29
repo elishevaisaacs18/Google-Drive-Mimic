d87df3fd3ab309cc0f1bd57e871c0f2df2ad89aa
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-29 13:02:15 +0200

    added users resources and get methods for specific user and all users

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fsPromises = require("fs/promises");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.sendFile(
+    "/home/hilma/HilmaCourse/Google-Drive-Mimic/Server/resources/users.json"
+  );
 });
+
+router.get("/:id", async function (req, res, next) {
+  const id = req.params.id;
+  try {
+    const result = await findUserBy(id);
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+  res.send();
+});
+
+async function findUserBy(id) {
+  try {
+    const rawData = await fsPromises.readFile(
+      `/home/hilma/HilmaCourse/Google-Drive-Mimic/Server/resources/users.json`,
+      "utf-8"
+    );
+    const data = JSON.parse(rawData)["users"];
+    let result = data.find((user) => user.id === parseInt(id));
+    if (!result) {
+      result = {};
+    }
+    return result;
+  } catch (err) {
+    throw new Error("Unable to read file");
+  }
+}
 
 module.exports = router;
