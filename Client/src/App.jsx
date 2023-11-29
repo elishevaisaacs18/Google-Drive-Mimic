71bcb54aabb247675fd66bea425bfc74a075964a
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-29 20:50:35 +0200

    delete and patch methods

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,16 +4,28 @@ import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import useFetch from "./assets/customHooks/useFetch";
 
 const App = () => {
+  const fetchData = useFetch;
+  async function sendRequestToDb(requestType, url, body) {
+    const response = await fetchData(url, {
+      method: requestType,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    return response;
+  }
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Navigate to="/login"></Navigate>} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login sendRequestToDb={sendRequestToDb}/>} />
         <Route path="/register" element={<Register />} />
         <Route element={<Layout />}>
-          <Route path="/home/:id" element={<Home />}></Route>
+          <Route path="/home/:id" element={<Home sendRequestToDb={sendRequestToDb} />}></Route>
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
--- Server/routes/content.js
+++ Server/routes/content.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const fsPromises = require("fs/promises");
+const path = require("path");
+const { readFile, writeFile } = require("fs/promises");
 
 router.get("/folder/:folderId", async function (req, res, next) {
-  //get all content in folder
   const folderId = req.params.folderId;
   try {
     const result = await findContentBy("currFolder", folderId);
@@ -23,19 +23,73 @@ router.get("/:id", async function (req, res, next) {
   }
 });
 
+router.delete("/:id", async function (req, res, next) {
+  const id = req.params.id;
+  try {
+    const contentToDelete = await findContentBy("id", id);
+    if (contentToDelete.length === 0) {
+      return res.status(404).send("Content not found");
+    }
+    const rawData = await readFile(
+      path.join(__dirname, "../resources/content.json"),
+      "utf-8"
+    );
+    const data = JSON.parse(rawData);
+    const updatedContent = data.content.filter((content) => content.id !== id);
+    data.content = updatedContent;
+    await writeFile(
+      path.join(__dirname, "../resources/content.json"),
+      JSON.stringify(data, null, 2),
+      "utf-8"
+    );
+    res.status(200).send(contentToDelete[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.patch("/:id", async function (req, res, next) {
+    const id = req.params.id;
+    try {
+      const fileToRename = await findContentBy("id", id);
+      if (fileToRename.length === 0) {
+        return res.status(404).send("Content not found");
+      }
+      const rawData = await readFile(
+        path.join(__dirname, "../resources/content.json"),
+        "utf-8"
+      );
+      const data = JSON.parse(rawData);
+      data["content"].find(content=>content.id == id).name = req.body.name
+      console.log('data: ', data)
+    //   const updatedContent = data.content.filter((content) => content.id !== id);
+    //   data.content = updatedContent;
+      await writeFile(
+        path.join(__dirname, "../resources/content.json"),
+        JSON.stringify(data, null, 2),
+        "utf-8"
+      );
+      const newFile = data["content"].find(content=>content.id == id)
+      res.status(200).send(newFile);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  });
+
 async function findContentBy(paramName, value) {
   try {
-    const rawData = await fsPromises.readFile(
+    const rawData = await readFile(
       path.join(__dirname, "../resources/content.json"),
       "utf-8"
     );
     const data = JSON.parse(rawData)["content"];
     let result = data.filter((content) => content[paramName] === value);
-    if (!result) {
+    if (result.length === 0) {
       result = {};
     }
     return result;
   } catch (err) {
+    console.error("Error reading file:", err.message);
     throw new Error("Unable to read file");
   }
 }
